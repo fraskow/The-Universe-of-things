302a28e180eb47fc7ca641611b2af8cc1d822e41
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-16 14:40:17 +0100

    Header style, footer style changes

--- src/assets/Ts/dateHour.ts
+++ src/assets/Ts/dateHour.ts
@@ -1,7 +1,6 @@
 export function getCurrentDateTime() {
   const currentDateTime:Date = new Date();
   const currentDateTimeFormatted:string = currentDateTime.toLocaleString();
-  console.log(currentDateTimeFormatted);
   const dateHourContainer: HTMLDivElement | null   = document.getElementById('dateHourContainer') as HTMLDivElement;
   dateHourContainer.textContent = currentDateTimeFormatted;
 }
