3b4e9beadb96238628ec0ecd91ea9840623c5ab0
Guillermo <guillermo.llausas@gmail.com>
2023-06-19 23:21:45 +0200

    galeria principal de heroes

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+export class GalleryComponent {
+  images: string[] = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg',
+    'image7.jpg',
+    'image8.jpg',
+    'image9.jpg',
+    'image10.jpg',
+    'image11.jpg',
+    'image12.jpg',
+  ];
+}
