1b8c20a4688dbe9666ecfd366b3888d46562d754
Francisco Manuel <fsaa93@gmail.com>
2023-06-21 15:48:21 +0200

    star fav done without functionality

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,21 +7,7 @@ import { SuperheroService } from '../superhero.service';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
-  images: string[] = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image5.jpg',
-    'image6.jpg',
-    'image7.jpg',
-    'image8.jpg',
-    'image9.jpg',
-    'image10.jpg',
-    'image11.jpg',
-    'image12.jpg',
-  ];
-
+  
   superheroes: any[] = [];
 
   constructor(private superheroService: SuperheroService) { }
