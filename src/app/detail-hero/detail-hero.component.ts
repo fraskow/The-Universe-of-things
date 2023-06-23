0d441c68fe9aec239e02f8ca68681bbbcc64c6cf
antonio <viz7mor@gmail.com>
2023-06-23 16:01:22 +0200

    branch timeline reverted. now branch details-section same level of develop branch

---
+++ src/app/detail-hero/detail-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-hero',
+  templateUrl: './detail-hero.component.html',
+  styleUrls: ['./detail-hero.component.scss']
+})
+export class DetailHeroComponent {
+  
+} 
